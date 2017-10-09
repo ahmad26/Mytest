a0e287355d7b1cb44ae45d8f6600a3accae2d683
ahmad26 <kucci@interia.pl>
2017-10-09 16:25:41 +0200

    edit1
    script updated

--- ScorePredictor.R
+++ ScorePredictor.R
@@ -7,9 +7,19 @@ serieA %>% select(HomeTeam, AwayTeam, FTHG, FTAG)
 homeData <- serieA %>% 
   select(HomeTeam, FTHG, FTAG) %>% 
   group_by(HomeTeam) %>% 
-  summarise(nrGames = n(), homeGoalsFor = sum(FTHG), meanHomeGoalsFor = mean(FTHG), homeGoalAgainst = sum(FTAG), meanHomeGoalsAgainst = mean(FTAG))
+  summarise(homeGoalsFor = sum(FTHG), meanHomeGoalsFor = mean(FTHG), homeGoalAgainst = sum(FTAG), meanHomeGoalsAgainst = mean(FTAG)) %>% 
+  mutate(HAP = homeGoalsFor/mean(homeGoalsFor), HDP = homeGoalAgainst/mean(homeGoalAgainst)) %>% 
+  rename(team = HomeTeam)
 
 awayData <- serieA %>% 
   select(AwayTeam, FTHG, FTAG) %>% 
   group_by(AwayTeam) %>% 
-  summarise(nrGames = n(), awayGoalsFor = sum(FTAG), meanAwayGoalsFor = mean(FTAG), awayGoalAgainst = sum(FTHG), awayHomeGoalsAgainst = mean(FTHG))
+  summarise(awayGoalsFor = sum(FTAG), meanAwayGoalsFor = mean(FTAG), awayGoalAgainst = sum(FTHG), meanAwayGoalsAgainst = mean(FTHG)) %>% 
+  mutate(AAP = awayGoalsFor/mean(awayGoalsFor), ADP = awayGoalAgainst/mean(awayGoalAgainst)) %>% 
+  rename(team = AwayTeam)
+
+teamPower <- inner_join(homeData,awayData,by="team")
+
+
+
+
